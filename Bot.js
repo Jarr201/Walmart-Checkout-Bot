41d091d52e775ec8626116a331a5977c530ea30d
Jalen Arrington <jmarrington178@yahoo.com>
2023-03-14 21:03:50 -0400

    Created givePage and addToCart functions + modified checkout function

--- Bot.js
+++ Bot.js
@@ -2,11 +2,20 @@ const puppeteer = require('puppeteer');
 
 const product_url = "https://www.walmart.com/ip/PNY-GeForce-RTX-4070-Ti-12GB-XLR8-Gaming-GPU-VERTO-EPIC-X-RGB-Triple-Fan-Graphics-Card-Nvidia-DLSS-3/1772848287?fulfillmentIntent=Shipping&athbdg=L1600"
 
-async function checkout() {
+async function givePage() {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
-    await page.goto(product_url);
     console.log('Page Pulled Up')
+    return page;
+}
+
+async function addToCart(page) {
+    await page.goto(product_url);
+}
+
+async function checkout() {
+    var page = await givePage();
+    await addToCart(page);
 }
 
 checkout()
